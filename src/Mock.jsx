dfbeda9af53d5ba49aa3e925c84b0abf9fdb5536
jmiguelmorralla <jmiguelmorralla@gmail.com>
2023-03-04 23:18:03 -0300

    item list e item list container

---
+++ src/Mock.jsx
@@ -0,0 +1,86 @@
+const listadoProductos = [
+    {
+       id:1,
+       nombre:"Balancín",
+       marca:"Juguetes Olsen",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:12000,
+       disponibilidad:true,
+       img: '/img/balancin.png'
+    },
+
+    {
+       id:2,
+       nombre:"Arcoiris",
+       marca:"Juguetes Olsen",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:7000,
+       disponibilidad:true,
+       img:"/img/arcoiris.png"
+    },
+    {
+       id:3,
+       nombre:"Casita",
+       marca:"Confiture",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:4500,
+       disponibilidad:true,
+       img:"/img/casita.png"
+    },
+    {
+       id:4,
+       nombre:"Bloques",
+       marca:"Tak Tak",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:3000,
+       disponibilidad:true,
+       img:"/img/bloques.png"
+    },
+    {
+       id:5,
+       nombre:"Torre",
+       marca:"Juguetes Olsen",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:2900,
+       disponibilidad:true,
+       img:"/img/torre.jpg"
+    },
+    {
+       id:6,
+       nombre:"Luna",
+       marca:"Confiture",
+       origen:"Argentina",
+       material:"Madera",
+       cantidad:1,
+       precio:5900,
+       disponibilidad:true,
+       img:"/img/luna.png"
+    }
+ ]
+
+ export const getFech = new Promise((respuesta, reject) => {
+    let url = "www.vitonio.com.ar";
+
+    if (url === "www.vitonio.com.ar") {
+        setTimeout(()=> {
+            respuesta(listadoProductos)}, 
+            2000       
+        )
+    } else {
+        reject("Fallo de conexión.")
+    }
+
+
+ })
+
+ export default listadoProductos;
