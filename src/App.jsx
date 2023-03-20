aafac228d5647bc5f83d04b8192abe196fffb95e
jmiguelmorralla <jmiguelmorralla@gmail.com>
2023-03-20 17:47:48 -0300

    context

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './components/NotFound/NotFound';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import cartContext from './context/cartContext';
 
 
 
@@ -13,17 +14,18 @@ function App() {
   return (
     
         <>
-        
-        <BrowserRouter>
-          <NavBar />
-            <Routes>
-              <Route path="/" element={<ItemListContainer />} />
-              <Route path="/items/:idProducto" element={<ItemDetailContainer />} />
-              <Route path="/category/:idCategory" element={<ItemListContainer />} />
+        <CartContextProvider value={}>
+          <BrowserRouter>
+            <NavBar />
+              <Routes>
+                <Route path="/" element={<ItemListContainer />} />
+                <Route path="/items/:idProducto" element={<ItemDetailContainer />} />
+                <Route path="/category/:idCategory" element={<ItemListContainer />} />
 
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-        </BrowserRouter>
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+          </BrowserRouter>
+        </CartContextProvider>
         </>
 
   );
